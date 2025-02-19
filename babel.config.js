2b200697b816ed6c0bb77db0f4192e9b2a237464
David <davidrappa@outlook.com>
2025-02-19 19:27:45 -0300

    packages: react-native-safe-area-context and babel-plugin-module-resolver dependencies

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["."],
+        alias: {
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@hooks": "./src/hooks",
+          "@services": "./src/services",
+        },
+      },
+    ],
+  ],
+};
