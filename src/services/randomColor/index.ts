bfecdb0541841f62695919601003163fe3d5dd9e
David Rappa <davidrappa@outlook.com>
2025-02-20 11:20:49 -0300

    feat: add function to set specific color with a dialog

--- src/services/randomColor/index.ts
+++ src/services/randomColor/index.ts
@@ -1,1 +1,2 @@
 export * from "./useRandomColor";
+export * from "./randomColorTypes";
--- src/services/randomColor/useRandomColor.ts
+++ src/services/randomColor/useRandomColor.ts
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { randomColorService } from "./randomColorService";
 
 import { useAppTheme } from "@hooks";
+import { DialogType } from "./randomColorTypes";
+import { delay } from "@utils";
 
 export function useRandomColor() {
   const { colors } = useAppTheme();
 
   const [backgroundColor, setBackgroundColor] = useState<string>(colors.white);
   const [isDarkColor, setIsDarkColor] = useState<boolean>(false);
+
+  const [isOpenDialog, setIsOpenDialog] = useState<DialogType | null>(null);
 
   function getRandomColor() {
     const color = randomColorService.generateRandomColor();
@@ -18,9 +22,31 @@ export function useRandomColor() {
     setBackgroundColor(color);
   }
 
+  function setSpecificColor(color: string) {
+    const isDark = randomColorService.verifyIfColorIsDark(color);
+
+    setIsDarkColor(isDark);
+    setBackgroundColor(color);
+  }
+
+  async function toggleColorPalletDialog(type: DialogType) {
+    if (type === isOpenDialog) {
+      return setIsOpenDialog(null);
+    }
+
+    setIsOpenDialog(type);
+
+    await delay(8000);
+
+    setIsOpenDialog(null);
+  }
+
   return {
     getRandomColor,
+    setSpecificColor,
+    toggleColorPalletDialog,
     backgroundColor,
     isDarkColor,
+    isOpenDialog,
   };
 }
