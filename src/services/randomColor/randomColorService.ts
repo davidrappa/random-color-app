e131d7832824d06cdc8ae8d048be0eba86e39ab0
David <davidrappa@outlook.com>
2025-02-19 21:38:59 -0300

    feat: add service to verify if color is dark

--- src/services/randomColor/randomColorService.ts
+++ src/services/randomColor/randomColorService.ts
@@ -12,6 +12,29 @@ function generateRandomColor(): string {
   return `#${getHexColor.padStart(6, "0")}`;
 }
 
+function convertHexToRGB(hexColor: string) {
+  const formatHex = hexColor.replace("#", "");
+
+  const r = parseInt(formatHex.substring(0, 2), 16);
+  const g = parseInt(formatHex.substring(2, 4), 16);
+  const b = parseInt(formatHex.substring(4, 6), 16);
+
+  return { r, g, b };
+}
+
+/**
+ * @description Verify if a color is dark
+ * @returns {boolean} True if the color is dark, false if is not
+ */
+function verifyIfColorIsDark(hexColor: string): boolean {
+  const { r, g, b } = convertHexToRGB(hexColor);
+
+  const brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+  return brightness < 128;
+}
+
 export const randomColorService = {
   generateRandomColor,
+  verifyIfColorIsDark,
 };
--- src/services/randomColor/useRandomColor.ts
+++ src/services/randomColor/useRandomColor.ts
@@ -4,15 +4,19 @@ import { randomColorService } from "./randomColorService";
 
 export function useRandomColor() {
   const [backgroundColor, setBackgroundColor] = useState<string>("#FFF");
+  const [isDarkColor, setIsDarkColor] = useState<boolean>(false);
 
   function getRandomColor() {
     const color = randomColorService.generateRandomColor();
+    const isDark = randomColorService.verifyIfColorIsDark(color);
 
+    setIsDarkColor(isDark);
     setBackgroundColor(color);
   }
 
   return {
     getRandomColor,
     backgroundColor,
+    isDarkColor,
   };
 }
